53671e207618df5990a32e2e225834f770eeeaba
Nihal Chaubey <59233625+chbey@users.noreply.github.com>
2024-06-14 10:06:43 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
+if __name__ == "__main__":
+    try:
+        # Input number for the Fibonacci series
+        number = int(input("Enter number for Fibonacci Series: "))
+        
+        if number <= 0:
+            print("Please enter a positive integer.")
+        else:
+            num1 = 0
+            num2 = 1
+            print(num1, num2, end=" ")
+            for i in range(2, number):
+                num3 = num1 + num2
+                print(num3, end=" ")
+                num1 = num2
+                num2 = num3
+    except ValueError:
+        print("Please enter a valid integer.")
 
-if __name__=='__main__':
-
-# Input number for the Fibonacci series
-    number = int(input("Enter number for fibonacci Series: "))
-
-    num1=0
-    num2=1
-    print(num1,num2 ,end=" ")
-    for i in range(2,number):
-        num3=num1+num2
-        print(num3,end=" ")
-        num1=num2
-        num2=num3
 
 
 
